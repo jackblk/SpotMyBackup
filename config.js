e78ca11edff875e864cf89aab805448bdf6438c0
Trung Le <jackblk4@gmail.com>
2021-12-03 21:37:52 +0700

    fix: correct oauth scope, refactor

---
+++ config.js
@@ -0,0 +1,8 @@
+config = {
+    "uri": "https://spot.haicon.moe",
+    "redirect_uri": "https://spot.haicon.moe/login.html",
+    "client_id": "30f12e314ef24aad8cf01a129c972431",
+    "scope": "playlist-read-collaborative playlist-read-private playlist-modify-public playlist-modify-private user-library-read user-library-modify",
+    "slowdown_import": 100,
+    "slowdown_export": 100
+};
